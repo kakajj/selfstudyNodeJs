b9d9e48d8b458cdf33c7014b209401ab67930a06
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-03-13 22:23:36 +0700

    [update] finish section 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,44 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
+
+// Define paths for Express config
 const publicDirPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../hbs/views');
+const partialsPath = path.join(__dirname,'../hbs/partials');
+// Setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
 
-
+// Setup static director to serve
 app.use(express.static(publicDirPath));
+
+// Routing handle
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name: 'weather.'
+    });
+});
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        example:'contact me : 080689641',
+        title: 'help',
+        name: 'JJ'
+    });
+});
+
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me?',
+        name: 'JJ Nachanon.'
+    });
+});
 
 app.get('/weather',(req,res)=>{
     res.send({
@@ -14,7 +47,20 @@ app.get('/weather',(req,res)=>{
     })
 });
 
+// Error when routing Handle
+app.get('/help/*',(req,res)=>{
+    res.render('errorPage',{
+        msg:'Help article not found...'
+    });
+});
 
+app.get('*',(req,res)=>{
+    res.render('errorPage',{
+        msg:'Page not found...'
+    });
+});
+
+// Setup port 
 app.listen(3000,()=>{
     console.log('Server is up now on port 3000....');
 });
